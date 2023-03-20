7048f4839ba9d9c1f844d0245b4912d1087892cf
TetianaTed <tetianated@gmail.com>
2023-03-20 20:14:41 +0100

    lessons with Michal: static method

---
+++ Contact.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaniaPraktyczneKursCSharpZUdemy
+{
+    internal class Contact
+    {
+        //statyczne pola dajemy na poczatku
+        private static string language = "pl";
+
+        //zasieg - scope (slowko public/private) dajemy na poczatku
+        private static List<Contact> contacts = new List<Contact>();
+
+        private string _name; // field
+        public string Name   // property
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _phoneNumber;  // Backing store
+
+        public string PhoneNumber
+        {
+
+            get { return _phoneNumber; }
+            set { _phoneNumber = value; }
+
+            //dodac weryfikacje
+            /*
+            get => _numerTelefonu;
+                   
+            set
+            {
+                if ((value > 0) && (value < 13))
+                {
+                    _numerTelefonu = value;
+                }
+            }
+            */
+
+        }
+
+        //Konstruktor piszemy za wlasciwosciami
+        //metoda ktora tworzy obiekt
+        //nazwa jak klasa, zacieg(scope) moze byc private lub public, internal, protected, virtual i inne
+
+        public Contact(string name, string phoneNumber)
+        {
+            this.Name = name;
+            this.PhoneNumber = phoneNumber;
+
+        }
+
+        //wypisuje wszytkie informacje, jakie zdefinuje, o stanie obiektu
+        // override ma nadpisac
+        public override string ToString()
+        {
+            //return "Contact (Name = " + this.Name + "; PhoneNumber = " + this.PhoneNumber + ")";
+            return $"Contact (Name = {this.Name}; PhoneNumber = {this.PhoneNumber}; Language = {language})"; //dla statycznych nie ma this
+        }
+
+        //static ma wplyw na wszystkie obiekty danego typu, (w tym przypadku Contact) bedacych w pamieci RAM (podczas dzialania programu)
+        public static Contact Test(string name)
+        {
+            //tu nie ma this
+            Contact.language = "bla bla";
+            return new Contact(name, "98561234");
+        }
+
+        public void Add(Contact newContact)
+        {
+            contacts.Add(newContact);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,78 @@
-﻿namespace ZadaniaPraktyczneKursCSharpZUdemy
+﻿using System.Globalization;
+using System.Xml.Linq;
+
+namespace ZadaniaPraktyczneKursCSharpZUdemy
 {
     internal class Program
     {
+        /*
+        Zadanie:
+        Utwórz aplikację konsolową która imituję zachowanie aplikacji z kontaktami na telefonie.
+        Kontakt to: nazwa i numer telefonu.
+        Aplikacja powinna:
+        Dodawać kontakty
+        Wyświetlić kontakt na podstawie numeru
+        Wyświetlić wszystkie kontakty
+        Wyszukać kontakty dla danej nazwy
+        */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Menu();
+
+
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Witam w ksiace telefonicznej. Proszę wybierz jedna z ponizszych opcji wprowadzajac odpowiednia liczbe: \n" +
+                "1 - Dodaj kontakt;\n" +
+                "2 - Wyswietl kontakt na podstawie numeru;\n" +
+                "3 - Wyswietl wszystkie kontakty;\n" +
+                "4 - Wyszukac kontakty dla danej nazwy.\n");
+
+            string wybranaOpcja = Console.ReadLine();
+            bool TryParseWybranaOpcja;
+            int wybranaOpcjaLiczbowa;
+            TryParseWybranaOpcja = int.TryParse(wybranaOpcja, out wybranaOpcjaLiczbowa);
+            if (wybranaOpcjaLiczbowa == 1)
+            {
+                DodajKontakt();
+            }
+        }
+
+        static void DodajKontakt()
+        {
+            Console.WriteLine("Wprowadz prosze nazwe kontaktu.");
+
+            string nazwaKontaktu = Console.ReadLine();
+
+            Console.WriteLine("Prosze wprowadz numer telefonu");
+
+            string numerTelefonu = Console.ReadLine();
+
+            List<Contact> primeNumbers = new List<Contact>()
+            { new Contact(name: "Ola", phoneNumber: "52325698745"),
+              new Contact(name: "Marcin", phoneNumber: "126455")
+              };
+
+            Console.WriteLine(primeNumbers.First().Name); //primeNumbers.First() = this w klasie contact, bo dzialam na obiekcie Ola
+
+            Console.WriteLine(primeNumbers.First()); // Console.WriteLine(primeNumbers.First().ToString()); ToString() robi sie domyslnie
+
+            Contact contact = new Contact(name: "Michal", phoneNumber: "123456789");
+            contact.ToString(); //  contact.ToString() = this w klasie contact, bo dzialam na obiekcie Michal
+            Contact testtik = Contact.Test("testowyString"); //jak statyczna metoda to nazwaklasy.nazwa metody. Nie wylolalam na obiekcie, nie bylo new. Nie bylo inicjilizacji obiektu, to nie ma this.
+            Console.WriteLine(testtik);
+
+            Console.WriteLine(primeNumbers.First().ToString());
+
+            /*
+            primeNumbers.Add(1,5);
+            primeNumbers.Add(1); // adding elements using add() method
+            primeNumbers.Add(3);
+            */
+
         }
     }
 }
