6416448749eebca6f66f199707c67b42c59f6f25
TetianaTed <tetianated@gmail.com>
2023-04-17 19:35:06 +0200

    Extract update validation to separate method

--- PhoneContact.cs
+++ PhoneContact.cs
@@ -56,22 +56,36 @@ namespace ZadaniaPraktyczneKursCSharpZUdemy
 
         public static void UpdateContactNumber(Contact oldContact, Contact newContact)
         {
-            IList<Contact> foundContacts = new List<Contact>();
+            string updateValidationResult = ValidateUpdate(newContact);
 
-            foreach (var contact in contacts)
+            if (!updateValidationResult.Equals(string.Empty))
             {
-                if (!string.IsNullOrWhiteSpace(oldContact.Name) && contact.Name.Equals(oldContact.Name,StringComparison.OrdinalIgnoreCase))
-                {
-                    foundContacts.Add(contact);
-                }
-                if (!string.IsNullOrWhiteSpace(oldContact.PhoneNumber) && 
-                                              (contact.PhoneNumber.Equals(oldContact.PhoneNumber, StringComparison.OrdinalIgnoreCase) 
-                                                && !oldContact.PhoneNumber.Equals("-1")))
-                {
-                    foundContacts.Add(contact);
-                }
+                Console.WriteLine("Blad walidacji: " + updateValidationResult);
+                return; //return; nie wykonuje ponizszego kodu metody
             }
 
+            if (!newContact.Name.Equals(string.Empty))
+            {
+                List<Contact> foundContacts = FindByName(newContact.Name);
+                if (foundContacts.Count == 1)
+                {
+                    Contact foundContact = foundContacts.First();
+                }
+            }
+            else if (!newContact.PhoneNumber.Equals(string.Empty))
+            {
+                Contact? foundContacts = FindByNumber(newContact.PhoneNumber);
+                if (foundContacts != null)
+                {
+                    Contact foundContact = foundContacts;
+                }
+            }
+            else
+            {
+                throw new Exception("Shoud never happened");
+            }
+            /*
+            IList<Contact> foundContacts = new List<Contact>();
 
             if (foundContacts.Count == 0)
             {
@@ -92,6 +106,7 @@ namespace ZadaniaPraktyczneKursCSharpZUdemy
                 }
 
             }
+            
 
             Contact? foundContact = FindByNumber(oldContact.PhoneNumber);
             if (foundContact == null)
@@ -103,6 +118,68 @@ namespace ZadaniaPraktyczneKursCSharpZUdemy
             {
                 Console.WriteLine($"Nie mozna poprawic kontakt, blad danych.");
             }
+            */
+        }
+
+        /* Warunki walidacji:
+         * Nazwa kontaktu ma byc unikalna
+         * Numer kontaktu musi byc unikalny
+         * Aktualizujac kontakt nalezy podac albo nazwe, albo numer (tylko jeden atrybut)
+         * 
+         * 
+         * */
+        private static string ValidateUpdate(Contact newContact)
+        {
+            if (newContact == null)
+            {
+                return "Kontakt nie moze byc pusty";
+            }
+            else if(string.IsNullOrEmpty(newContact.Name) && string.IsNullOrEmpty(newContact.PhoneNumber))
+            {
+                return "Numer i nazwa kontaktu nie moga byc jednoczesnie puste";
+            }            
+            else if (string.IsNullOrWhiteSpace(newContact.Name))
+            {
+                ValidateUpdateByNumber(newContact.PhoneNumber);
+            }
+            else if (string.IsNullOrWhiteSpace(newContact.PhoneNumber))
+            {
+                ValidateUpdateByName(newContact.Name);
+            }
+            else
+            {
+                return "Jednoczesna aktualizacja numeru i nazwy kontaktu nie jest wspierana";
+            }
+
+            
+            return string.Empty;
+        }
+
+        private static string ValidateUpdateByNumber(string newPhoneNumber)
+        {
+            foreach (var contact in contacts)
+            {
+
+                if (contact.PhoneNumber.Equals(newPhoneNumber))
+                {
+                    return "Taki numer telefonu juz istnieje";
+                }
+            }
+            return string.Empty;
+        }
+
+        private static string ValidateUpdateByName(string newName)
+        {
+            foreach (var contact in contacts)
+            {
+
+                if (contact.Name.Equals(newName))
+                {
+                    return "Taka nazwa kontaktu juz istnieje";
+                }
+            }
+            return string.Empty;
+
         }
     }
 }
